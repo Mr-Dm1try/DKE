356ac0dea1a489147c4640c26f68bd0c54b3e0b8
Dmitry <mr.dmitriy99@yandex.ru>
2020-07-29 16:53:50 +0300

    Added an output schema

---
+++ DataModel/Output/OutputEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModel.Output
+{
+    public class OutputEntry
+    {
+        public int BuyOrderId { get; set; }
+        public int SellOrderId { get; set; }
+        public long Volume { get; set; }
+        public long Value { get; set; }
+    }
+}
---
+++ DataModel/Output/OutputSchema.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModel.Output
+{
+    public class OutputSchema
+    {
+        public bool IsOk { get; set; }
+        public long Price { get; set; }
+        public long FullPrice { get; set; }
+        public List<OutputEntry> OutputEnties { get; set; }
+    }
+}
