db82a998e2b63d1a30392b373f702ecfce94e9c1
Dmitry <mr.dmitriy99@yandex.ru>
2020-07-29 16:50:03 +0300

    Added Input schtma

---
+++ DataModel/Input/InputEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace DataModel.Input
+{
+    public class InputEntry
+    {
+        public int Id { get; set; }
+        public bool IsBuy { get; set; }
+        public bool IsMarket { get; set; }
+        public long Volume { get; set; }
+        public long? Value { get; set; }
+    }
+}
---
+++ DataModel/Input/InputSchema.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataModel
+{
+    public class InputSchema
+    {
+        List<InputEntry> BuyEntries { get; set; }
+        List<InputEntry> SellEntries { get; set; }
+    }
+}
