6bdd3aa8b0bd6426b62d94dfc7fe5b3961d815a4
Dmitry <mr.dmitriy99@yandex.ru>
2020-07-29 17:17:05 +0300

    Added a serialeze class

---
+++ CsvHelper/OutputSerialization.cs
@@ -0,0 +1,16 @@
+﻿using DataModel.Output;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsvHelper
+{
+    public class OutputSerialization
+    {
+        public void SerializeToFile(string filePath, OutputSchema outputSchema)
+        {
+
+        }
+    }
+}
